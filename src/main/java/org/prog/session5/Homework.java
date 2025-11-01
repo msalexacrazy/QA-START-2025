29f0d85012f7f0fe3069cc80e45f32e25ab72a64
Oleksandra Sokolovska <Oleksandra.Sokolovska@raiffeisen.ua>
2025-11-01 21:56:26 +0200

    Automation Homework_5 collections

---
+++ src/main/java/org/prog/session5/Homework.java
@@ -0,0 +1,39 @@
+package org.prog.session5;
+
+import java.sql.Array;
+import java.util.*;
+
+//TODO: write Map with car owners as keys and owned cars as values
+//TODO: assign each car random color using randomColor() from this class
+
+public class Homework {
+    public static void main(String[] args) {
+        Map<String, List<HomeworkCar>> mapCarOwner = new HashMap<>();
+
+        String[] owners = new String[]{"Anton", "Sveta", "Serg", "Nina", "Sasha"};
+
+        for (String owner : owners) {
+            mapCarOwner.put(owner, new ArrayList<>());
+        }
+
+        for (String owner : owners) {
+            List<HomeworkCar> cars = mapCarOwner.get(owner);
+            Random random = new Random();
+            int carsCount = random.nextInt(5) + 1;
+
+            for (int i = 0; i < carsCount; i++) {
+                HomeworkCar car = new HomeworkCar();
+                car.color = HomeworkCar.randomColor();
+                cars.add(car);
+            }
+        }
+
+        for (Map.Entry<String, List<HomeworkCar>> entry : mapCarOwner.entrySet()) {
+            System.out.println("Owner " + entry.getKey() + " has cars:");
+            for (HomeworkCar car : entry.getValue()) {
+                System.out.println("   - color: " + car.color);
+            }
+        }
+    }
+}
+
---
+++ src/main/java/org/prog/session5/HomeworkCar.java
@@ -0,0 +1,20 @@
+package org.prog.session5;
+
+import java.util.Random;
+
+public class HomeworkCar {
+    public String color;
+
+    public static String randomColor() {
+        Random random = new Random();
+        return switch (random.nextInt(5)) {
+            case 0 -> "red";
+            case 1 -> "green";
+            case 2 -> "blue";
+            case 3 -> "yellow";
+            case 4 -> "pink";
+            default -> "black";
+        };
+    }
+
+}

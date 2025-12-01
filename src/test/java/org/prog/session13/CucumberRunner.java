ea888b4f5f1be3f76fb61b4c9e4dc78b3e22b852
Oleksandra Sokolovska <Oleksandra.Sokolovska@raiffeisen.ua>
2025-12-01 22:51:47 +0200

    Automation Homework_13

--- src/test/java/org/prog/session13/CucumberRunner.java
+++ src/test/java/org/prog/session13/CucumberRunner.java
@@ -30,7 +30,7 @@ public class CucumberRunner extends AbstractTestNGCucumberTests {
     @BeforeSuite
     public void beforeSuite() throws ClassNotFoundException, SQLException, MalformedURLException {
         WebSteps.driver = new RemoteWebDriver(
-                new URL("http://selenium_grid:4444/wd/hub"), remoteChrome());
+                new URL("http://selenium-hub:4444/wd/hub"), remoteChrome());
         Class.forName("com.mysql.cj.jdbc.Driver");
         DBSteps.connection = DriverManager.getConnection(
                 "jdbc:mysql://mysql_db:3306/db", "root", "password");

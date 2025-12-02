4a5a46889d952bea514c635ad0890809262843d2
Oleksandra Sokolovska <Oleksandra.Sokolovska@raiffeisen.ua>
2025-12-02 23:31:42 +0200

    Automation Homework_13 Allure

--- src/test/java/org/prog/session13/AllureSampleTests.java
+++ src/test/java/org/prog/session13/AllureSampleTests.java
@@ -23,7 +23,7 @@ public class AllureSampleTests {
     @BeforeSuite
     public void initWebDriver() throws MalformedURLException {
         driver = WebSteps.driver = new RemoteWebDriver(
-                new URL("http://selenoid:4445/wd/hub"), CucumberRunner.remoteChrome());
+                new URL("http://selenium-hub:4444/wd/hub"), CucumberRunner.remoteChrome());
         googlePage = new GooglePage(driver);
         w3SchoolsPage = new W3SchoolsPage(driver);
     }
